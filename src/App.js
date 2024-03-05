02bba3eef2490209aaa809e3aaa6df262de68c38
devloper45 <113242732+devloper45@users.noreply.github.com>
2024-03-05 10:08:33 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import AuthInputs from './components/AuthInputs.js';
+import Header from './components/Header.js';
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  );
+}
